a51ef2893503af60acb36eb096b212d14c0a6d41
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-12-26 23:02:20 -0300

    Começando com Stream e buffers no C#

---
+++ ByteBankIO/LendoArquivos.cs
@@ -0,0 +1,34 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void LendoArquivosDiretamenteComBuffers(string[] args)
+    {
+        string enderecoArquivo = "D://Cursos/ByteBankIO-master/contas.txt";
+
+
+        using (FileStream fs = new FileStream(enderecoArquivo, FileMode.Open))
+        {
+            byte[] buffer = new byte[1024];
+            int numeroDeBytes = -1;
+
+            while (numeroDeBytes != 0)
+            {
+                numeroDeBytes = fs.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer, numeroDeBytes);
+            }
+
+            fs.Close();
+            Console.WriteLine();
+        }
+
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+        string texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,25 @@
 ﻿using ByteBankIO;
+using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
+        string enderecoArquivo = "D://Cursos/ByteBankIO-master/contas.txt";
 
-        Console.ReadLine();
+        using (var fs = new FileStream(enderecoArquivo, FileMode.Open))
+        {
+            StreamReader leitor = new StreamReader(fs);
+
+            //string linha = leitor.ReadLine();
+            //string texto = leitor.ReadToEnd();
+
+            while(!leitor.EndOfStream)
+            {
+                Console.WriteLine(leitor.ReadLine());
+            }
+        }
+
     }
+
 }
